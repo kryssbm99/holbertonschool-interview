18dd1838438322a5d998d5dcaa556ea844bcdae1
Kryss <kryssbm99@gmail.com>
2025-01-02 14:41:56 -0500

    Applying fixes

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""
+The program checks if all boxes can be unlocked
+and returns True if they can, False otherwise
+"""
+
+
 def canUnlockAll(boxes):
     """
     Determines if all boxes can be unlocked
@@ -8,15 +14,15 @@ def canUnlockAll(boxes):
         True if all boxes can be unlocked, False otherwise
     """
     n = len(boxes)
-    unlocked = [False] * n
-    unlocked[0] = True
-    keys = [0]
+    unlocked = [False] * n  # list of booleans to track if a box is unlocked
+    unlocked[0] = True  # first box is unlocked
+    keys = [0]  # list of keys to unlock boxes
 
     while keys:
-        current_key = keys.pop()
+        current_key = keys.pop()  # get the current key
         for key in boxes[current_key]:
             if 0 <= key < n and not unlocked[key]:
                 unlocked[key] = True
-                keys.append(key)
+                keys.append(key)  # add key to list of keys to unlock boxes
 
-    return all(unlocked)
+    return all(unlocked)  # check if all boxes are unlocked
